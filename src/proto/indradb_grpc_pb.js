5af72141f127f6ed71e05d5c9c1b626cd1dbe90a
djKooks <inylove82@gmail.com>
2023-05-16 21:26:50 +0900

    Cleanup codegen command

--- src/proto/indradb_grpc_pb.js
+++ src/proto/indradb_grpc_pb.js
@@ -3,6 +3,7 @@
 'use strict';
 var grpc = require('@grpc/grpc-js');
 var indradb_pb = require('./indradb_pb.js');
+var google_protobuf_timestamp_pb = require('google-protobuf/google/protobuf/timestamp_pb.js');
 var google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb.js');
 
 function serialize_google_protobuf_Empty(arg) {
--- src/proto/indradb_pb.d.ts
+++ src/proto/indradb_pb.d.ts
@@ -2,6 +2,7 @@
 // file: indradb.proto
 
 import * as jspb from "google-protobuf";
+import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
 import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
 
 export class Uuid extends jspb.Message {
